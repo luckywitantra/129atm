4f9b69cd0d411257c8815602fc5211d8bf7b5b13
luckywitantra <iceman.sum41@gmail.com>
2026-08-21 15:29:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbwnLjfPZrOM21ln6-crxdnGebqHUQSXInpk6sa5kzxatf9vhUFsZakMFDyr-UxTCUM_/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzawSNj45jOqdyEFKhF79w6-uW_MJansgX5c-nEoQ-aimJWCbxkH7lRNBYTVFAEp4VI/exec';
 
 // ==========================================
 // 1. STATE MANAGEMENT, THEME & PAGINATION
